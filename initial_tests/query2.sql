29886b404f5ab2fcdeb9a0faf5d8861ab7e81a45
DiMesq <d.mesquita285@gmail.com>
2015-12-02 16:53:35 +0000

    Remove queries that achieved the same purpose.

--- initial_tests/query2.sql
+++ initial_tests/query2.sql
@@ -1,17 +1,4 @@
 # What are the notes (registo table) that appear in all pages (page table) of a certain user?
-SELECT 	R.userid, R.typid, R.nome
-FROM 	registo R
-WHERE 	NOT EXISTS(	SELECT 	P.pagecounter
-					FROM	pagina P
-					WHERE 	P.userid = R.userid
-							AND P.pagecounter 	NOT IN (SELECT 	RP.pageid
-														FROM 	reg_pag RP
-														WHERE 	RP.userid = R.userid 
-																AND RP.typeid = R.typecounter
-																AND RP.regid = R.regcounter)
-);
-
-# outra forma para o 2 (acho que esta é melhor)
 SELECT 	R.userid, R.typeid, R.nome
 FROM 	registo R
 WHERE 	NOT EXISTS(	SELECT 	P.pagecounter
