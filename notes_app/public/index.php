ec175617ac1bd30736241d429ee39d5bd5366a4c
DiMesq <d.mesquita285@gmail.com>
2015-12-05 17:39:13 +0000

    Put some holder code in the index just to test.

--- notes_app/public/index.php
+++ notes_app/public/index.php
@@ -1,3 +1,6 @@
 <?php
 	
+	require_once("../utils/config.php");
+
+	render("user.php", ["title" => "Home"]);
 ?>
---
+++ notes_app/template/user.php
@@ -0,0 +1,3 @@
+<div class="row row-content">
+	<h1>Hello, world! I'm the user.</h1>
+</div>
