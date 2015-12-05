0c0a44994c7e22f44d70ee7d1845b7047b3b891f
DiMesq <d.mesquita285@gmail.com>
2015-12-05 13:02:50 +0000

    Add the header and footer files.

--- notes_app/template/header.php
+++ notes_app/template/header.php
@@ -11,9 +11,11 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap-theme.min.css" rel="stylesheet">
     <link href="css/bootstrap-social.css" rel="stylesheet">
+    <!-- Font-awesome -->
     <link href="css/font-awesome.min.css" rel="stylesheet">
-
+    <!-- Custom styling -->
     <link href="css/mystyles.css" rel="stylesheet">
+
 </head>
 
 <body>
@@ -26,26 +28,25 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Ristorante Con Fusion</a>
+                <a class="navbar-brand" href="#">Notify</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>   Home</a></li>
-                    <li><a href="#"><i class="fa fa-info"></i> About</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expandend="false"><i class="fa fa-cutlery"></i> Menu<span class="caret"></span></a>
-                         <ul class="dropdown-menu">
-                            <li><a href="#">Appetizers</a></li>
-                            <li><a href="#">Main Courses</a></li>
-                            <li><a href="#">Desserts</a></li>
-                            <li><a href="#">Drinks</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li class="dropdown-header">Specials</li>
-                            <li><a href="#">Lunch Buffet</a></li>
-                            <li><a href="#">Weekend Brunch</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="contactus.html"><i class="fa fa-phone"></i> Contact</a></li>
+                    <?php if (!empty(SESSION["id"])): ?>
+                        <li class="active"><a href="#"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>   Home</a></li>
+                        <li><a href="#"><i class="fa fa-folder"></i> Types</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#"><i class="fa fa-clone"></i> Pages</a></li>
+                    <?php endif; ?>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <?php if (empty(SESSION["id"])): ?>
+                        <li><a href="#"></i> Login</a></li>
+                        <li><a href="#"></i><Strong> Register</Strong></a></li>
+                    <?php else: ?>
+                        <li><a href="#"><i class="fa fa-sign-out"></i> Logout</a></li>
+                    <?php endif; ?>
                 </ul>
             </div>
         </div>
