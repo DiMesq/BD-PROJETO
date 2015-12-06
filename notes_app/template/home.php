636d4f6d8617bd5f8509f593e00a483eb64878ef
DiMesq <d.mesquita285@gmail.com>
2015-12-06 12:02:05 +0000

    Creating the landing page template.

--- notes_app/template/home.php
+++ notes_app/template/home.php
@@ -1,3 +1,11 @@
-<div class='row row-content'>
-	<h1> Notify </h1>
+<div class="jumbotron vertical-center">
+	<div class="container">
+		<div class="row row-header">
+			<div class="col-xs-12 col-sm-4 col-sm-offset-4">
+				<a class="btn btn-success btn-lg round-edge" href="register.php" role="button"><h1>Notify</h1></a>
+				<p style="padding: 20px;">Your own personalized Note Book</p>
+			</div>
+			<div class="col-xs-12 col-sm-4"></div>
+		</div>
+	</div>
 </div>
