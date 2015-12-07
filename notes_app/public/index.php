346ced0759aa70f4ab5b2ae95c383eb7a51fa770
DiMesq <d.mesquita285@gmail.com>
2015-12-07 19:25:47 +0000

    Add the js file for the registration.

--- notes_app/public/index.php
+++ notes_app/public/index.php
@@ -2,14 +2,9 @@
 	
 	require_once("../utils/config.php");
 
-
 	// TODO: query paginas
 
 	// TODO: query tipos
 
-
-
-
-
-	render("user.php", ["title" => "Home", "results" => $paginas_result]);
+	render("user.php", ["title" => "Home"]);//, "results" => $paginas_result]);
 ?>
