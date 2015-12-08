86756c953349cda61cda1b90fa8f1e2537aad8d4
DiMesq <d.mesquita285@gmail.com>
2015-12-08 08:58:35 +0000

    Shows the users types in the main user page.

--- notes_app/template/user.php
+++ notes_app/template/user.php
@@ -24,3 +24,29 @@
         </div>
     </div>
 </div>
+<div class="container">
+	<div class="row row-content">
+        <div class="col-xs-12 col-sm-9">
+            <h2>Your Types</h2>
+            <div class="table-responsive">
+		        <table class="table table-striped">
+		        	<thead>
+		            <tr>
+		                <th>Nome</th>
+		            </tr>
+		            </thead>
+		            <tbody>
+		            <?php foreach ($types as $type): ?>
+			            <tr>
+			                <td><?=$type['nome']?></td>
+			            </tr>
+			        <?php endforeach; ?>
+		            </tbody>
+		        </table>
+		    </div>
+        </div>
+         <div class="col-xs-12 col-sm-3">
+            <p style="padding:20px;"></p>
+        </div>
+    </div>
+</div>
