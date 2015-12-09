f57cef9e11fab80c3e733d93200ac8e38247df9f
DiMesq <d.mesquita285@gmail.com>
2015-12-09 15:01:50 +0000

    Take into account if the things are active or not in the second query.

--- answers_to_questions/query2.sql
+++ answers_to_questions/query2.sql
@@ -1,15 +1,18 @@
 # What are the notes (registo table) that appear in all pages (page table) of a certain user?
 SELECT 	R.userid, R.typecounter, R.nome
 FROM 	registo R
-WHERE 	NOT EXISTS(	SELECT 	P.pagecounter
-					FROM	pagina P
-					WHERE 	P.userid = R.userid
-							AND NOT EXISTS(		SELECT 	RP.pageid
-												FROM 	reg_pag RP
-												WHERE 	RP.userid = R.userid 
-														AND RP.typeid = R.typecounter
-														AND RP.regid = R.regcounter
-														AND RP.pageid = P.pagecounter)
+WHERE 	R.ativo = true
+		AND NOT EXISTS(	SELECT 	P.pagecounter
+						FROM	pagina P
+						WHERE 	P.userid = R.userid
+								AND P.ativa = true
+								AND NOT EXISTS(		SELECT 	RP.pageid
+													FROM 	reg_pag RP
+													WHERE 	RP.userid = R.userid 
+															AND RP.typeid = R.typecounter
+															AND RP.regid = R.regcounter
+															AND RP.pageid = P.pagecounter
+															AND RP.ativa = true)
 );
 
 
