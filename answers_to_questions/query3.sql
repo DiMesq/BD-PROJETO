d362f233e7781242a6c420eeb4e7f9bde22895a6
DiMesq <d.mesquita285@gmail.com>
2015-12-09 16:25:28 +0000

    fix bug: make query 3 take into account if stuff is active or not and fix the average to count for pages without notes.

--- answers_to_questions/query3.sql
+++ answers_to_questions/query3.sql
@@ -1,17 +1,55 @@
 # What are the users that have the biggest average of notes per page?
 SELECT	U.nome, U.email
 FROM 	utilizador U
-		NATURAL JOIN   (SELECT 		counts.userid, AVG(counts.n_reg) as reg_average
-						FROM 	   (SELECT 		RP.userid, RP.pageid, COUNT(*) as n_reg
-									FROM		reg_pag RP
-									GROUP BY 	RP.userid, RP.pageid) as counts
-						GROUP BY 	counts.userid) as averages
-WHERE averages.reg_average = (	SELECT	MAX(A2.reg_average)
-								FROM   (SELECT 		counts.userid, AVG(counts.n_reg) as reg_average
-										FROM 	   (SELECT 		RP.userid, RP.pageid, COUNT(*) as n_reg
-													FROM		reg_pag RP
-													GROUP BY 	RP.userid, RP.pageid) as counts
-										GROUP BY 	counts.userid) as A2);
+		NATURAL JOIN   (SELECT 		reg_per_page.userid, (SUM(reg_per_page.n_reg) / MAX(page_per_user.n_pag)) as reg_average
+						FROM 	   (SELECT 		P.userid, P.pagecounter, COUNT(*) as n_reg
+									FROM		pagina P, reg_pag RP, registo R, tipo_registo T
+									WHERE 		RP.userid = P.userid
+												AND RP.pageid = P.pagecounter
+												AND RP.userid = R.userid
+												AND RP.regid = R.regcounter
+												AND RP.typeid = R.typecounter
+												AND RP.userid = T.userid
+												AND RP.typeid = T.typecnt
+												AND RP.ativa = true
+												AND P.ativa = true
+												AND R.ativo = true
+												AND T.ativo = true
+									GROUP BY 	P.userid, P.pagecounter) as reg_per_page
+								
+									NATURAL JOIN 	
+									               (SELECT 		userid, COUNT(*) as n_pag
+												 	FROM 		pagina 
+												 	WHERE 		ativa = true
+												 	GROUP BY	userid) as page_per_user
+
+						GROUP BY 	reg_per_page.userid) as A2
+
+WHERE averages.reg_average = (	SELECT		MAX(A2.reg_average)
+								FROM   	   (SELECT 		reg_per_page.userid, (SUM(reg_per_page.n_reg) / MAX(page_per_user.n_pag)) as reg_average
+											FROM 	   (SELECT 		P.userid, P.pagecounter, COUNT(*) as n_reg
+														FROM		pagina P, reg_pag RP, registo R, tipo_registo T
+														WHERE 		RP.userid = P.userid
+																	AND RP.pageid = P.pagecounter
+																	AND RP.userid = R.userid
+																	AND RP.regid = R.regcounter
+																	AND RP.typeid = R.typecounter
+																	AND RP.userid = T.userid
+																	AND RP.typeid = T.typecnt
+																	AND RP.ativa = true
+																	AND P.ativa = true
+																	AND R.ativo = true
+																	AND T.ativo = true
+														GROUP BY 	P.userid, P.pagecounter) as reg_per_page
+													
+														NATURAL JOIN   (SELECT 		userid, COUNT(*) as n_pag
+																	 	FROM 		pagina 
+																	 	WHERE 		ativa = true
+																	 	GROUP BY	userid) as page_per_user
+
+											GROUP BY 	reg_per_page.userid) as A2);
+
+						
 
 
 
